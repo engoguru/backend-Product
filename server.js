dbee6606fa2672bf6867b327fe52888796b67896
Anuj Kumar <xp27anujkumar@gmail.com>
2025-10-08 16:00:07 +0530

    banner api created

--- server.js
+++ server.js
@@ -7,10 +7,11 @@ import connectdb from './db/connectDB.js';
 const app=express();
 import productRoutes from './routes/productRoutes.js'
 import productFeedbackRoutes from './routes/productFeedbackRoutes.js'
+import bannerRoutes from './routes/bannerRoutes.js';
 const PORT=process.env.PORT || 5002;
 
 app.use(cors({
-  origin: 'http://localhost:5173', // ✅ Frontend URL
+  origin: ['http://localhost:5173', 'http://localhost:5174'], // ✅ Frontend URL
   credentials: true                // ✅ Required to send cookies/JWT
 }));
 app.use(express.json());
@@ -21,6 +22,7 @@ app.get('/productHealth',(_,res)=>res.json({ok:true,message:"Healthy !"}));
 
 app.use('/productList',productRoutes);
 app.use('/productFeedback',productFeedbackRoutes);
+app.use('/banner',bannerRoutes);
 
 
 app.listen(PORT,()=>{
